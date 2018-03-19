62ec3bf88c49b1e42b5758db04d8acf0a3da28bf
nsected@gmail.com <nsected@gmail.com>
2018-03-19 19:28:04 +0500

    test

--- libse/allureStub.js
+++ libse/allureStub.js
@@ -1,19 +1,19 @@
 module.exports =
     function () {
-        // if (global.allure === undefined) {
-        //     global.allure = {};
-        //     global.allure.createAttachment = _ => {
-        //         return _ => {
-        //         }
-        //     };
-        //     global.allure.description = _ => {
-        //         return _ => {
-        //         }
-        //     };
-        //     global.allure.createStep = function (step, callback) {
-        //         return _ => {
-        //             callback()
-        //         }
-        //     };
-        // }
+        if (global.allure === undefined) {
+            global.allure = {};
+            global.allure.createAttachment = _ => {
+                return _ => {
+                }
+            };
+            global.allure.description = _ => {
+                return _ => {
+                }
+            };
+            global.allure.createStep = function (step, callback) {
+                return _ => {
+                    callback()
+                }
+            };
+        }
     };
